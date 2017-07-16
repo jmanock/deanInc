f5ef34f1826a4669960c83cb8a17dd241339a763
Jon Manock <jonmanock@gmail.com>
2017-07-16 09:31:25 -0400

    starting from scratch

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-// generated on 2017-04-18 using generator-webapp 2.4.1
+// generated on 2017-07-16 using generator-webapp 3.0.1
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync').create();
@@ -9,7 +9,7 @@ const runSequence = require('run-sequence');
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
-var dev = true;
+let dev = true;
 
 gulp.task('styles', () => {
   return gulp.src('app/styles/*.css')
