b7589609669c7fa872e1d5e957ce6603f8c4c514
Jon Manock <jonmanock@gmail.com>
2017-07-28 11:59:01 -0400

    asterisk show hide works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  $('.info').on('change', function(){
+    var something = this.id;
+    something = '.'+something;
+    if($(this).val().length > 0){
+      $(something).removeClass('fa-asterisk');
+    }else{
+      $(something).addClass('fa-asterisk');
+    }
+  });
+});
